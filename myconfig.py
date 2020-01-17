281548c74d485bf02901af1092edf731a4934824
John <john@john>
2020-01-17 21:44:05 +0800

    windows sync

--- myconfig.py
+++ myconfig.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 
-ynote_bak_path = r'C:\Users\Zhe Zhang\Desktop\yd\final\youdao_note_20170303'
+ynote_bak_path = r'C:\Users\John\Documents\my-work\onesnote\data\youdao_note_20200117'
 
